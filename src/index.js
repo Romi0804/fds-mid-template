a2387047bb0b688c0580ac798288b2457beb55a1
Ham Choromi <choromi0804@gmail.com>
2018-11-08 14:50:38 +0900

    서버연결하여 구조완성

--- src/index.js
+++ src/index.js
@@ -15,8 +15,14 @@ api.interceptors.request.use(function (config) {
   return config
 });
 
+//템플릿목록
 const templates = {
+  layout: document.querySelector('#layout').content,
   loginForm: document.querySelector('#login-form').content,
+  productList: document.querySelector('#product-list').content,
+  productItem: document.querySelector('#product-item').content,
+  productDetail: document.querySelector('#product-detail').content,
+  detailImage: document.querySelector('#detail-image').content
 }
 
 const rootEl = document.querySelector('.root')
@@ -32,43 +38,159 @@ const rootEl = document.querySelector('.root')
 // 5. 이벤트 리스너 등록하기
 // 6. 템플릿을 문서에 삽입
 
-async function drawLoginForm() {
-// 1. 템플릿 복사
-const frag = document.importNode(templates.loginForm, true)
-//현재문서가 아닌 외부 html의 문서의 노드를 복사해와서 현재문서에 넣어준다.
+//메인화면 구현
+// fragment를 받아서 layout에 넣은 다음 rootEl에 그려주는 함수
+function drawFragment(frag) {
+  //1. 템플릿 복사
+  const layoutFrag = document.importNode(templates.layout, true)
 
-// 2. 요소 선택
-const formEl = frag.querySelector('.login-form')
-//form class를 불러서 설정한 요소를 지정해준다.
+  //2. 요소 선택
+  const mainEl = layoutFrag.querySelector('.main')
 
-// 3. 필요한 데이터 불러오기 - 필요없음
-// 4. 내용 채우기 - 필요없음
-
-// 5. 이벤트 리스너 등록하기
-  formEl.addEventListener('submit', async e => {
-    e.preventDefault()
-    const username = e.target.elements.username.value
-    const password = e.target.elements.password.value
-
-    const res = await api.post('/users/login', {
-      username,
-      password
-    })
-  //server주소에 username과 password를 써서 /users/login/경로로
-  //api.post 를 이용하여 보내면
-  //awiat 응답을 기다린다.
-  //res 응답이 온다.
-
-    localStorage.setItem('token', res.data.token)
-    //The setItem() method of the Storage interface,
-    //when passed a key name and value, will add that key to the storage,
-    //or update that key's value if it already exists.
-    //위에서 res라는 응답객체에 .data.token을 붙이면 응답과 함께 실려온 토큰을 꺼내올수 있어영
-    //그걸 꺼내서 'token' 이라는 곳에 넣어서 local Storage에 저장을 한다는 말씀!
-    DrawLoginAfter()
-    //로그인 한후 뜨는 화면을 그리기 (지금 현재는 내가 임의 지정)
-  })
-// 6. 템플릿을 문서에 삽입
+  //6.템플릿을 문서에 삽입
+  mainEl.appendChild(frag)
+  //메인화면에 있는 버튼을 누르면 그 안에서 화면을 변하게 하는 것
   rootEl.textContent = ''
-  rootEl.appendChild(frag)
+  rootEl.appendChild(layoutFrag)
+  //변화된 전체 화면 출력
 }
+
+//상품목록구현
+async function drawProductList(category) {
+  // 1. 템플릿 복사
+  const frag = document.importNode(templates.productList, true)
+
+  // 2. 요소 선택
+  const productListEl = frag.querySelector('.product-list')
+
+  // 3. 필요한 데이터 불러오기
+  const params = {}
+  if (category) {
+    params.category = category
+  }
+  const response = await api.get('/products', {
+    params
+  })
+  console.log(response)
+  const productList = response.data
+  // /products 경로로 api.get 을 이용하여 받아온다.
+  // await 응답을 기다린다.
+  // productList 데이터가 온다.
+
+  // 4. 내용 채우기
+  for (const { id: postId, title, description, mainImgUrl } of productList) {
+    //
+    // 1. 템플릿 복사
+    const frag = document.importNode(templates.productItem, true)
+
+    // 2. 요소 선택
+    const productItemEl = frag.querySelector('.product-item')
+    const mainImageEl = frag.querySelector('.main-image')
+    const titleEl = frag.querySelector('.title')
+    const descriptionEl = frag.querySelector('.description')
+
+    // 3. 필요한 데이터 불러오기 - x
+    // 4. 내용 채우기
+    mainImageEl.setAttribute('src', mainImgUrl)
+    //src라는 이름에 mainImg Url을 넣어라
+
+    titleEl.textContent = title
+    descriptionEl.textContent = description
+
+    // 5. 이벤트 리스너 등록하기
+    productItemEl.addEventListener('click', e => {
+      drawPostDetail(postId)
+    })
+
+    // 6. 템플릿을 문서에 삽입
+    productListEl.appendChild(frag)
+  }
+  // 5. 이벤트 리스너 등록하기
+  // 6. 템플릿을 문서에 삽입
+  drawFragment(frag)
+}
+
+//로그인폼 기능 구현
+async function drawLoginForm() {
+  // 1. 템플릿 복사
+  const frag = document.importNode(templates.loginForm, true)
+  //현재문서가 아닌 외부 html의 문서의 노드를 복사해와서 현재문서에 넣어준다.
+
+  // 2. 요소 선택
+  const formEl = frag.querySelector('.login-form')
+  //form class를 불러서 설정한 요소를 지정해준다.
+
+  // 3. 필요한 데이터 불러오기 - 필요없음
+  // 4. 내용 채우기 - 필요없음
+
+  // 5. 이벤트 리스너 등록하기
+    formEl.addEventListener('submit', async e => {
+      e.preventDefault()
+      const username = e.target.elements.username.value
+      const password = e.target.elements.password.value
+
+      const res = await api.post('/users/login', {
+        username,
+        password
+      })
+    //server주소에 username과 password를 써서 /users/login/경로로
+    //api.post 를 이용하여 보내면
+    //awiat 응답을 기다린다.
+    //res 응답이 온다.
+
+      localStorage.setItem('token', res.data.token)
+      //The setItem() method of the Storage interface,
+      //when passed a key name and value, will add that key to the storage,
+      //or update that key's value if it already exists.
+      //위에서 res라는 응답객체에 .data.token을 붙이면 응답과 함께 실려온 토큰을 꺼내올수 있어영
+      //그걸 꺼내서 'token' 이라는 곳에 넣어서 local Storage에 저장을 한다는 말씀!
+      DrawLoginAfter()
+      //로그인 한후 뜨는 화면을 그리기 (지금 현재는 내가 임의 지정)
+    })
+  // 6. 템플릿을 문서에 삽입
+    rootEl.textContent = ''
+    rootEl.appendChild(frag)
+  }
+
+  //상품 상세
+ async function drawPostDetail(productId) {
+  // 1. 템플릿 복사
+  const frag = document.importNode(templates.productDetail, true)
+
+  // 2. 요소 선택
+  const mainImageEl = frag.querySelector('.main-image')
+  const titleEl = frag.querySelector('.title')
+  const descriptionEl = frag.querySelector('.description')
+  const cartFormEl = frag.querySelector('.cartForm')
+  const detailImageListEl = frag.querySelector('.detail-image-list')
+
+  // 3. 필요한 데이터 불러오기
+  const { data: {
+    title,
+    description,
+    mainImgUrl,
+    detailImgUrls
+  } } = await api.get(`/products/${productId}`)
+
+  console.log(detailImgUrls)
+  // 4. 내용 채우기
+  mainImageEl.setAttribute('src', mainImgUrl)
+  titleEl.textContent = title
+  descriptionEl.textContent = description
+  // for (const url of detailImgUrls) {
+  //   const frag = document.importNode(templates.detailImage, true)
+
+  //   const detailImageEl = frag.querySelector('.detail-image')
+
+  //   detailImageEl.setAttribute('src', url)
+
+  //   detailImageListEl.appendChild(frag)
+  // }
+
+  // 5. 이벤트 리스너 등록하기
+  // 6. 템플릿을 문서에 삽입
+  drawFragment(frag)
+}
+
+drawProductList()
+
